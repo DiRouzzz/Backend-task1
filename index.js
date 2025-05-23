98d696b28c8652fd34b5704cad0831d783a482e3
Oleg Portnov <drose1@list.ru>
2025-05-23 20:55:19 +0400

    createdServer

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { addNote, printNotes, removeNote } from './notes.controller.js';
+import http from 'node:http';
+import chalk from 'chalk';
+import fs from 'node:fs/promises';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { addNote } from './notes.controller.js';
 
-const version = '1.0.1';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-yargs(hideBin(process.argv))
-	.version(version)
-	.command({
-		command: 'add',
-		describe: 'Add new note to list',
-		builder: {
-			title: {
-				type: 'string',
-				describe: 'Note title',
-				demandOption: true,
-			},
-		},
-		handler({ title }) {
+const port = 3000;
+const basePath = path.join(__dirname, 'pages');
+
+const server = http.createServer(async (req, res) => {
+	if (req.method === 'GET') {
+		const content = await fs.readFile(path.join(basePath, 'index.html'));
+
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+		res.end(content);
+	} else if (req.method === 'POST') {
+		const body = [];
+
+		res.writeHead(200, {
+			'Content-Type': 'text/plain; charset=utf-8',
+		});
+
+		req.on('data', data => {
+			body.push(Buffer.from(data));
+		});
+
+		req.on('end', () => {
+			const title = body.toString().split('=')[1].replaceAll('+', ' ');
 			addNote(title);
-		},
-	})
-	.command({
-		command: 'list',
-		describe: 'Print all notes',
-		async handler() {
-			printNotes();
-		},
-	})
-	.command({
-		command: 'remove',
-		describe: 'Remove note by id',
-		builder: {
-			id: {
-				type: 'string',
-				describe: 'Note id',
-				demandOption: true,
-			},
-		},
-		async handler({ id }) {
-			removeNote(id);
-		},
-	})
-	.parse();
+
+			res.end(`Title = ${title}`);
+		});
+	}
+});
+
+server.listen(port, () => {
+	console.log(chalk.green(`Server has been started ${port}...`));
+});
